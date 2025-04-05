119d88544ac335e99b394fcbfb423bb96952f704
Annika Damstedt <acdamstedt@ucsb.edu>
2025-04-05 09:46:29 -0700

    xy - updated with acutal team name and members

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -32,6 +32,25 @@ public class DeveloperTest {
     public void getGithubId_returns_correct_githubId() {
         assertEquals("acdamstedt", Developer.getGithubId());
     }
+
+    @Test
+    public void getTeam_returns_team_with_correct_name() {
+        Team  t = Developer.getTeam();
+        assertEquals("s25-07", t.getName());
+    }
+
+    @Test
+    public void getTeam_returns_team_with_correct_members() {
+        Team  t = Developer.getTeam();
+        assertTrue(t.getMembers().contains("Annika D"),"Team should contain Annika D");
+        assertTrue(t.getMembers().contains("Hannah Z"),"Team should contain Hannah Z");
+        assertTrue(t.getMembers().contains("Forrest Z"),"Team should contain Forrest Z");
+        assertTrue(t.getMembers().contains("Hannya Y"),"Team should contain Hannya Y");
+        assertTrue(t.getMembers().contains("Shelly Z"),"Team should contain Shelly Z");
+        assertTrue(t.getMembers().contains("Steven L"),"Team should contain Steven L");
+    }
+
+
     
 
 }
